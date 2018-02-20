505a80e7a611d3ffbf779d0ee5fac9a0abe733be
Peter Vlugter <pvlugter@gmail.com>
2018-02-21 11:49:26 +1300

    Add more variations to the demo

--- src/main/scala/sample/CinnamonAkkaStreamsDemo.scala
+++ src/main/scala/sample/CinnamonAkkaStreamsDemo.scala
@@ -18,15 +18,21 @@ object CinnamonAkkaStreamsDemo extends App {
 
   def delay(i: Int): Int = { Thread.sleep(i); i }
 
-  def increase(i: Int): Int = i * random(10)
+  def random(min: Int, max: Int): Int = {
+    val mean = (max + min) / 2.0
+    val stddev = (max - min) / 8.0
+    math.round(math.min(math.max(ThreadLocalRandom.current.nextGaussian * stddev + mean, min), max)).toInt
+  }
 
-  def random(k: Int): Int = math.min(math.abs(ThreadLocalRandom.current.nextGaussian * k).toInt, 4 * k) + 1
+  def increase(i: Int): Int = i * random(min = 1, max = 10)
 
   def probability(p: Double): Boolean = ThreadLocalRandom.current.nextDouble < p
 
+  def flaky(i: Int): Int = if (probability(0.1)) throw new RuntimeException("flaky") else i
+
   // stage functions
 
-  val generate = () => Iterator.continually(random(10))
+  val generate = () => Iterator.continually(random(min = 1, max = 10))
 
   val one = (i: Int) => delay(i)
 
@@ -36,14 +42,33 @@ object CinnamonAkkaStreamsDemo extends App {
 
   // instrumented streams
 
-  Source.fromIterator(generate)
-    .map(one).named("one")
-    .filter(two)
-    .via(Flow[Int].map(three).named("three"))
-    .to(Sink.ignore)
-    .named("first")
-    .instrumented(reportByName = true)
-    .run()
+  // method to select in configuration
+  def stream() = {
+    Source.fromIterator(generate)
+      .map(one)
+      .map(three)
+      .to(Sink.ignore)
+      .run()
+  }
+
+  // multiple instances of the stream
+  stream()
+  stream()
+
+  // method to select in configuration
+  def first() = {
+    Source.fromIterator(generate)
+      .map(one).named("one")
+      .via(Flow[Int].filter(two).named("two"))
+      .via(Flow[Int].map(three).named("three"))
+      .to(Sink.ignore)
+      .named("first")
+      .run()
+  }
+
+  // multiple instances of the "first" stream
+  first()
+  first()
 
   Source.fromIterator(generate)
     .mapAsync(4)(i => FutureNamed("second.one")(one(i)))
@@ -61,15 +86,24 @@ object CinnamonAkkaStreamsDemo extends App {
     .instrumented(name = "third")
     .run()
 
-  Source.fromIterator(generate)
-    .groupBy(2, _ < 5)
-    .map(one)
-    .filter(two)
-    .map(three)
-    .mergeSubstreams
-    .to(Sink.ignore)
-    .instrumented(name = "fourth", reportByInstance = true)
-    .run()
+  Source.fromIterator(() => Iterator.continually(random(min = 1, max = 5))).flatMapConcat { n =>
+    Source(List.fill(n)(random(min = 1, max = 20))).mapAsync(n) { i =>
+      Source.single(i)
+        .map(i => flaky(delay(increase(i))))
+        .recover { case _: Exception => 0 }
+        .toMat(Sink.head)(Keep.right)
+        .instrumented(name = "fourth")
+        .run()
+    }
+  }.runWith(Sink.ignore)
+
+  Source.fromIterator(() => Iterator.continually(random(min = 10, max = 50))).flatMapConcat { n =>
+    Source(List.fill(n)(random(min = 50, max = 100))).mapAsync(n) { i =>
+      implicit val executionContext = system.dispatchers.lookup("small-dispatcher")
+      FutureNamed("fifth.one")(one(i))
+        .mapNamed("fifth.three")(three)
+    }
+  }.runWith(Sink.ignore)
 
   println("Press enter to exit ...")
   System.in.read()
